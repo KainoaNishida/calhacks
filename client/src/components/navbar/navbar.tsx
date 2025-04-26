a601c06fb1f38f6f7c9e324298f5cc4248c74d7f
Kainoa Nishida <kanishid@uci.edu>
2025-04-26 16:10:08 -0700

    installed and added material ui to the project

---
+++ client/src/components/navbar/navbar.tsx
@@ -0,0 +1,47 @@
+import { ChevronLeftIcon } from '@chakra-ui/icons';
+import { Button, IconButton } from '@chakra-ui/react';
+import PropTypes from 'prop-types';
+import { Link, useNavigate } from 'react-router-dom';
+
+function Navbar({ showContactUs, title, showSettings, excelDownload, backButton }) {
+  const navigate = useNavigate();
+  const businessList = [
+    { name: 'Contact Us', path: '/ContactUs' },
+    { name: 'Settings', path: '/EditContactInformation' },
+    { name: 'Excel Download', path: '/Download' }, // Download not made yet
+    { name: 'Back Button', path: '/' },
+  ];
+
+  return (
+    <>
+      <nav>
+        <IconButton
+          aria-label="Back button"
+          icon={<ChevronLeftIcon />}
+          onClick={() => navigate('/')}
+        />
+        {showContactUs && (
+          <Button>
+            <Link to={businessList[0].path}>{businessList[0].name}</Link>
+          </Button>
+        )}
+        {showSettings && (
+          <Button>
+            <Link to={businessList[1].path}>{businessList[1].name}</Link>
+          </Button>
+        )}
+        {excelDownload && (
+          <Button>
+            <Link to={businessList[2].path}>{businessList[2].name}</Link>
+          </Button>
+        )}
+      </nav>
+    </>
+  );
+}
+
+Navbar.propTypes = {
+  isAdmin: PropTypes.bool.isRequired,
+};
+
+export default Navbar;
